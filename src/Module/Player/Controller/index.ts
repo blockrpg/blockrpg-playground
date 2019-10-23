42760648be82d14fa23e2a796f9df6bafd372f67
鸡毛 <shihao.gu@perfma.com>
2019-10-23 13:47:56 +0800

    跟随框架更新

--- src/Module/Player/Controller/index.ts
+++ src/Module/Player/Controller/index.ts
@@ -1,24 +1,21 @@
 import Router from 'koa-router';
 import { Rsp } from 'blockrpg-core/built/Koa/Rsp';
 import { Session } from 'blockrpg-core/built/Session';
+import { PlayerMeta } from 'blockrpg-core/built/Model/PlayerMeta/Entity';
 import * as RoamerBLL from 'blockrpg-core/built/Model/Roamer/BLL';
 
 const router = new Router();
 
-// 查询玩家当前信息
-// 包含玩家基本信息以及Roamer信息
+// 查询玩家在数据库中持久化的Roamer信息
 router.post('/api/playground/player/curinfo', async (ctx, next) => {
   const session = ctx.cookies.get('session') as string;
-  const metaInfo = await Session.Get(session);
-  const roamerInfo = await RoamerBLL.getRoamerBLL(metaInfo.account);
-  if (!roamerInfo) {
-    Rsp.Error(ctx, 500, '无法获取玩家位置信息');
-    return;
+  const meta = await Session.Get(session) as PlayerMeta;
+  const roamer = await RoamerBLL.upgradeRoamerBLL(meta.Account);
+  if (roamer) {
+    Rsp.Success(ctx, roamer);
+  } else {
+    Rsp.Error(ctx, 500, '无法获取玩家Roam信息');
   }
-  Rsp.Success(ctx, {
-    ...metaInfo,
-    ...roamerInfo,
-  });
 });
 
 export default router.routes();
